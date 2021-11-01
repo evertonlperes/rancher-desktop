9e173ebfcccbb17edbd52ca98f4c27fe60f9118c
evertonlperes <evertonluperes@gmail.com>
2021-11-01 14:59:28 -0600

    Debug: increase timeout process bar discovery
    Signed-off-by: evertonlperes <evertonluperes@gmail.com>

--- e2e/k8s.e2e.spec.ts
+++ e2e/k8s.e2e.spec.ts
@@ -81,7 +81,7 @@ describe('Rancher Desktop - K8s Sample Deployment Test', () => {
 
     app.client.saveScreenshot('./it-02.png');
     // Wait for the progress bar to exist
-    // await progress.waitForExist({ timeout: 15000 });
+    await progress.waitForExist({ timeout: 30000 });
     // Wait for progress bar to disappear again
     await progress.waitForExist({ timeout: 600000, reverse: true });
 
