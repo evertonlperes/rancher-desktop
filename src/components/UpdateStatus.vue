2afd2d8682c6ae334d004ff46ab4d1ae94c093fb
evertonlperes <evertonluperes@gmail.com>
2021-11-04 09:30:10 -0600

    Debug: List rancher process in CI - small trick updateState - not null
    Signed-off-by: evertonlperes <evertonluperes@gmail.com>

--- src/components/UpdateStatus.vue
+++ src/components/UpdateStatus.vue
@@ -47,7 +47,7 @@ const UpdateStatusProps = Vue.extend({
       default: false,
     },
     updateState: {
-      type:    Object as PropType<UpdateState | null>,
+      type:    Object as PropType<UpdateState | 'not found'>,
       default: null,
     },
     version: {
