d73e08d5f55cc5d206df9c7b9be3c1b9c21ee66d
evertonlperes <evertonluperes@gmail.com>
2021-12-07 12:57:44 -0600

    Minor changes playwright config
    Signed-off-by: evertonlperes <evertonluperes@gmail.com>

--- e2e/config/playwright.config.ts
+++ e2e/config/playwright.config.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
-import type { Config, PlaywrightTestOptions } from '@playwright/test';
+import type { Config, PlaywrightTestOptions, PlaywrightWorkerOptions } from '@playwright/test';
 
 const outputDir = path.join(__dirname, '..', 'e2e', 'test-results');
 const testDir = path.join(__dirname, '..', '..', 'e2e');
 
-const config: Config<PlaywrightTestOptions> = {
+const config: Config< & PlaywrightWorkerOptions & PlaywrightTestOptions> = {
   testDir,
   outputDir,
   timeout:       process.env.CI ? 600_000 : 300_000,
