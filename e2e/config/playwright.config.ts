2e5bcaceb5ba48bb6d17f032400992c1e0d65279
evertonlperes <evertonluperes@gmail.com>
2021-12-09 09:47:16 -0600

    Added more stuff pw config
    Signed-off-by: evertonlperes <evertonluperes@gmail.com>

--- e2e/config/playwright.config.ts
+++ e2e/config/playwright.config.ts
@@ -7,11 +7,12 @@ const testDir = path.join(__dirname, '..', '..', 'e2e');
 const config: Config< & PlaywrightWorkerOptions & PlaywrightTestOptions> = {
   testDir,
   outputDir,
-  timeout:       process.env.CI ? 600_000 : 300_000,
+  timeout:       !!process.env.CI ? 600_000 : 300_000,
   globalTimeout: 600_000,
-  retries:       process.env.CI ? 2 : undefined,
+  retries:       !!process.env.CI ? 2 : undefined,
   workers:       1,
   reporter:      'list',
+  use:           { navigationTimeout: 40_000 }
 };
 
 export default config;
