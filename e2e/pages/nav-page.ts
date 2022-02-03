07b82e8eda8cab9a58c78c06c4e4d61c193c98b7
Everton Peres <37411123+evertonlperes@users.noreply.github.com>
2022-02-03 10:55:35 -0300

    waitfor wait for dom
    Signed-off-by: Everton Peres <37411123+evertonlperes@users.noreply.github.com>

--- e2e/pages/nav-page.ts
+++ e2e/pages/nav-page.ts
@@ -46,7 +46,9 @@ export class NavPage {
 
     async navigateTo(tab: keyof typeof pageConstructors) {
       await this.page.click(`.nav li[item="/${ tab }"] a`);
-      await this.page.waitForNavigation({ url: `**/${ tab }`, timeout: 120_000 });
+      await this.page.waitForNavigation({
+        url: `**/${ tab }`, timeout: 60_000, waitUntil: 'domcontentloaded'
+      });
 
       return pageConstructors[tab](this.page);
     }
