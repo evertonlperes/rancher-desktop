827d7ba57b1e86d9b59f9708422159d056926855
Everton Peres <37411123+evertonlperes@users.noreply.github.com>
2022-02-17 18:06:28 -0600

    Increase a bit more the coverage Array class
    Signed-off-by: Everton Peres <37411123+evertonlperes@users.noreply.github.com>

--- src/utils/__tests__/array.spec.ts
+++ src/utils/__tests__/array.spec.ts
@@ -3,9 +3,18 @@ import * as array from '@/utils/array';
 describe('Array', () => {
   test('should remove an Object', async() => {
     const dummyArray = ['test', 'dummy'];
-    const test = await array.removeObject(dummyArray, 'dummy');
+    const result = await array.removeObject(dummyArray, 'dummy');
 
-    expect(test).toEqual(expect.arrayContaining(['test']));
-    expect(test).not.toEqual(expect.arrayContaining(['dummy']));
+    expect(result).toEqual(expect.arrayContaining(['test']));
+    expect(result).not.toEqual(expect.arrayContaining(['dummy']));
+  });
+
+  test('should remove many Objects', async() => {
+    const dummyArray = ['test', 'dummy', 'dunno', 'denno'];
+    const objs = ['dunno', 'denno'];
+    const result = await array.removeObjects(dummyArray, objs);
+
+    expect(result).toEqual(expect.arrayContaining(['test', 'dummy']));
+    expect(result).not.toEqual(expect.arrayContaining(['dunno', 'denno']));
   });
 });
